1d9dc97efb1441edc5682fedd8599dd9fcd76dce
leoCardosoDev <leocardosodev@gmail.com>
2023-10-24 08:44:58 -0300

    feat: add checkout stock use case

---
+++ src/modules/product_adm/usecase/check_stock/check_stock_usecase.ts
@@ -0,0 +1,18 @@
+import ProductGateway from "../../gateway/product_gateway";
+import { CheckStockInputDto, CheckStockOutputDto } from "./check_stock_dto";
+
+export default class CheckStockUseCase {
+  private _productRepository: ProductGateway
+
+  constructor (productRepository: ProductGateway) {
+    this._productRepository = productRepository
+  }
+
+  async execute(input: CheckStockInputDto): Promise<CheckStockOutputDto> {
+    const product = await this._productRepository.find(input.productId)
+    return {
+      productId: product.id.id,
+      stock: product.stock
+    }
+  }
+}

2587fe5fc76566f7c8a772b412baafa8b93ce9e1
leoCardosoDev <leocardosodev@gmail.com>
2023-10-24 15:51:48 -0300

    feat: add FindProductUsecase

---
+++ src/modules/store_catalog/usecase/find_product/find_product_dto.ts
@@ -0,0 +1,10 @@
+export interface FindProductOutputDto {
+  id: string
+  name: string
+  description: string
+  salesPrice: number
+}
+
+export interface FindProductInputDto {
+  id: string
+}
---
+++ src/modules/store_catalog/usecase/find_product/find_product_usecase.ts
@@ -0,0 +1,18 @@
+import Id from "../../../@shared/domain/value_object/id_value_object";
+import UseCaseInterface from "../../../@shared/usecase/use_case_interface";
+import ProductGateway from "../../gateway/product_gateway";
+import { FindProductInputDto, FindProductOutputDto } from "./find_product_dto";
+
+export default class FindProductUseCase implements UseCaseInterface {
+  constructor (private productRepository: ProductGateway) {}
+
+  async execute(input: FindProductInputDto): Promise<FindProductOutputDto> {
+    const product = await this.productRepository.find(input.id)
+    return {
+      id: product.id.id,
+      name: product.name,
+      description: product.description,
+      salesPrice: product.salesPrice
+    }
+  }
+}

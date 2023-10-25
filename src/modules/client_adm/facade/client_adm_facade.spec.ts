96481fdd8ce2e782cb93a4b56bc065d5ed90749e
leoCardosoDev <leocardosodev@gmail.com>
2023-10-25 15:19:05 -0300

    feat: create client facade find

--- src/modules/client_adm/facade/client_adm_facade.spec.ts
+++ src/modules/client_adm/facade/client_adm_facade.spec.ts
@@ -3,6 +3,7 @@ import { ClientModel } from "../repository/client_model"
 import ClientRepository from "../repository/client_repository"
 import AddClientUseCase from "../usecase/add_client/add_client_usecase"
 import ClientAdmFacade from "./client_adm_facade"
+import FindClientUseCase from "../usecase/find_client/find_client_usecase"
 
 describe('Client Adm Facade test', () => {
   let sequelize: Sequelize
@@ -21,6 +22,7 @@ describe('Client Adm Facade test', () => {
   afterEach(async () => {
     await sequelize.close()
   })
+
   test('Should create a client', async () => {
     const repository = new ClientRepository()
     const addUsecase = new AddClientUseCase(repository)
@@ -41,6 +43,32 @@ describe('Client Adm Facade test', () => {
     expect(client.dataValues.name).toBe(input.name)
     expect(client.dataValues.email).toBe(input.email)
     expect(client.dataValues.address).toBe(input.address)
+  })
+
+  test('Should find a Client', async () => {
+    const repository = new ClientRepository()
+    const findUsecase = new FindClientUseCase(repository)
+    const addUsecase = new AddClientUseCase(repository)
+    const facade = new ClientAdmFacade({
+      addUsecase: addUsecase,
+      findUsecase: findUsecase
+    })
+
+    const input = {
+      id: '1',
+      name: 'CLient 1',
+      email: 'test@test.com',
+      address: 'Address 1'
+    }
+
+    await facade.add(input)
+
+    const client = await facade.find({ id: '1' })
+    expect(client).toBeDefined()
+    expect(client.id).toBe(input.id)
+    expect(client.name).toBe(input.name)
+    expect(client.email).toBe(input.email)
+    expect(client.address).toBe(input.address)
   })
 })
 

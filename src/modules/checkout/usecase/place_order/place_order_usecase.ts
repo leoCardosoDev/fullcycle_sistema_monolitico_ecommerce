dd70ca543de6429fb6edaea76341ed2d83e27457
leoCardosoDev <leocardosodev@gmail.com>
2023-10-26 09:03:12 -0300

    feat: add PlaceOrderUsecase Test

--- src/modules/checkout/usecase/place_order/place_order_usecase.ts
+++ src/modules/checkout/usecase/place_order/place_order_usecase.ts
@@ -1,11 +1,19 @@
 import UseCaseInterface from '../../../@shared/usecase/use_case_interface'
+import ClientAdmFacadeInterface from '../../../client_adm/facade/client_adm_facade_interface'
 import { PlaceOrderInputDto, PlaceOrderOutputDto } from './place_order_dto'
 
 export default class PlaceOrderUseCase implements UseCaseInterface {
+  private _clientFacade: ClientAdmFacadeInterface
 
-  constructor () {}
+  constructor (clientFacade: ClientAdmFacadeInterface) {
+    this._clientFacade = clientFacade
+  }
 
   async execute(input: PlaceOrderInputDto): Promise<PlaceOrderOutputDto> {
+    const client = await this._clientFacade.find({ id: input.clientId })
+    if (!client) {
+      throw new Error('Client not found')
+    }
     return {
       id: '',
       invoiceId: '',
